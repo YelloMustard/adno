9c346582162d2d312a4475e76d348c56883f2d2f
Marco Primitivo <marco.primitivo@gmail.com>
2012-12-22 18:21:13 +0100

    Added addthis inside colorbox.

---
+++ sites/all/modules/custom/adno_custom/tpl/addthis.tpl.php
@@ -0,0 +1,14 @@
+<div class="social-media-plugin" id="social-share-plugin">
+  <div class="addthis_toolbox addthis_default_style" addthis:url="<?php print $page_url; ?>" addthis:description="Big kisses from Combell Island">
+    <a class="addthis_button_preferred_1"></a>
+    <a class="addthis_button_preferred_2"></a>
+    <a class="addthis_button_preferred_3"></a>
+    <a class="addthis_button_preferred_4"></a>
+    <a class="addthis_button_preferred_5"></a>
+    <a class="addthis_button_preferred_6"></a>
+    <a class="addthis_button_preferred_7"></a>
+    <a class="addthis_button_compact"></a>
+    <a class="addthis_button_facebook_like"></a>
+  </div>
+  <script type="text/javascript" src="http://s7.addthis.com/js/250/addthis_widget.js#pubid=ra-4df0db6c5993110f"></script>
+</div>
