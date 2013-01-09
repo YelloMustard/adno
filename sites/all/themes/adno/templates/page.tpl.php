6641d24295a6cc6dee7ea837bb62ad057214e92a
Marijn De Smedt <freekymage@gmail.com>
2013-01-09 03:50:37 +0100

    Updated styling: forms, doormat, tabs, gallery.

--- sites/all/themes/adno/templates/page.tpl.php
+++ sites/all/themes/adno/templates/page.tpl.php
@@ -18,41 +18,31 @@
       <div id="content" <?php if (isset($page['sidebar_right']) && $page['sidebar_right']) {
           print 'class="right-sidebar-enabled"';
         }?>>
-        <div id="inner_content">
-          <?php print render($title_prefix); ?>
-          <?php if ($title): ?>
-            <h1 id="page-title" class="title"><?php print $title; ?></h1>
-          <?php endif; ?>
-          <?php print render($title_suffix); ?>
 
-          <?php if ($messages): ?>
-            <div id="messages"><?php print $messages; ?></div>
-          <?php endif; ?>
+        <?php print render($title_prefix); ?>
+        <?php if ($title): ?>
+          <h1 id="page-title" class="title"><?php print $title; ?></h1>
+        <?php endif; ?>
+        <?php print render($title_suffix); ?>
 
-          <?php if ($tabs): ?>
-            <div class="tabs"><?php print render($tabs); ?></div>
-          <?php endif; ?>
+        <?php if ($messages): ?>
+          <div id="messages"><?php print $messages; ?></div>
+        <?php endif; ?>
 
-          <?php print render($page['help']); ?>
+        <?php if ($tabs): ?>
+          <div class="tabs"><?php print render($tabs); ?></div>
+        <?php endif; ?>
 
-          <?php if (isset($action_links) && $action_links): ?>
-            <ul class="action-links"><?php print render($action_links); ?></ul>
-          <?php endif; ?>
-            <div class="content-inner">
-              <!-- AddThis Button BEGIN -->
-              <div class="addthis_toolbox addthis_default_style ">
-                <a class="addthis_button_preferred_1"></a>
-                <a class="addthis_button_preferred_2"></a>
-                <a class="addthis_button_preferred_3"></a>
-                <a class="addthis_button_preferred_4"></a>
-                <a class="addthis_button_compact"></a>
-                <a class="addthis_counter addthis_bubble_style"></a>
-              </div>
-              <script type="text/javascript" src="//s7.addthis.com/js/300/addthis_widget.js#pubid=xa-50cdc8f750e09a4c"></script>
-              <!-- AddThis Button END -->
-              <?php print render($page['content']); ?>
-           </div>
+        <?php print render($page['help']); ?>
+
+        <?php if (isset($action_links) && $action_links): ?>
+          <ul class="action-links"><?php print render($action_links); ?></ul>
+        <?php endif; ?>
+
+        <div class="content-inner">
+          <?php print render($page['content']); ?>
         </div>
+
       </div>
       <?php if (isset($page['sidebar_right']) && $page['sidebar_right']): ?>
         <div id="sidebar-right" class="sidebar">
